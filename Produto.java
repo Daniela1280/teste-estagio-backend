0eae587f0599c29377b7c34636dc3f0aebcba2ab
Daniela1280 <80360957+Daniela1280@users.noreply.github.com>
2025-06-06 18:11:23 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -2,19 +2,20 @@ public class Produto {
   String nome;
   double preco;
   int quantidade;
-  public Produto(String nome, double preco, int quantidade)
-  { 
+
+  public Produto(String nome, double preco, int quantidade) { 
     this.nome = nome;
     this.preco = preco;
     this.quantidade = quantidade;
   }
+
   public void vender(int qtd) {
     this.quantidade -= qtd;
   }
+
   public String toString() {
-    return nome +"-R$" + preco + " - Estoque: " + quantidade;
+    return nome + "-R$" + preco + " - Estoque: " + quantidade;
   }
 }
-  
-  
+
   
