1088f62d8d5e282fdd2b113e2720feb13dd8621d
Daniela1280 <80360957+Daniela1280@users.noreply.github.com>
2025-06-06 18:10:42 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -2,12 +2,15 @@ public class Usuario {
   String nome;
   String cpf;
   String email;
-public Usuario (String nome, String cpf, String email)
-  { this.nome = nome;
+
+  public Usuario(String nome, String cpf, String email) {
+    // Corrigido: estava 'Nome' maiúsculo, deve ser 'nome'
+    this.nome = nome;
     this.cpf = cpf;
     this.email = email;
   }
+
   public String toString() {
-    return nome +"-"+ cpf +"-"+ email;
+    return nome + "-" + cpf + "-" + email;
   }
-  }
+}
