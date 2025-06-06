09b8e3a65e678965652d509d33de1c49fe9cd61d
Daniela1280 <80360957+Daniela1280@users.noreply.github.com>
2025-06-06 18:12:07 -0300

    Update ItemVenda.java

--- ItemVenda.java
+++ ItemVenda.java
@@ -1,18 +1,18 @@
 public class ItemVenda {
-    Produto produto;
-    int quantidade;
+  Produto produto;
+  int quantidade;
 
-    public ItemVenda(Produto produto, int quantidade) {
-        this.produto = produto;
-        this.quantidade = quantidade;
-        produto.vender(quantidade);
-    }
+  public ItemVenda(Produto produto, int quantidade) {
+    this.produto = produto;
+    this.quantidade = quantidade;
+    produto.vender(quantidade);
+  }
 
-    public double getTotal() {
-        return produto.preco * quantidade;
-    }
+  public double getTotal() {
+    return produto.preco * quantidade;
+  }
 
-    public String toString() {
-        return produto.nome + " x" + quantidade + " = R$" + getTotal();
-    }
+  public String toString() {
+    return produto.nome + " x" + quantidade + " = R$" + getTotal();
+  }
 }
